b21febca443566f9169512addb27586b502f3ae2
NeoTokyoUnicorn <neotokyounicorn@gmail.com>
2022-06-01 16:46:27 +0200

    added map

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,5 +1,15 @@
-const Map = () => {
-    return <h1>Map</h1>;
+import React from 'react';
+import ImageMapper from 'react-img-mapper';
+
+const Map = props => {
+  const URL = 'https://raw.githubusercontent.com/img-mapper/react-docs/master/src/assets/example.jpg';
+  const MAP = {
+    name: 'my-map',
+    // GET JSON FROM BELOW URL AS AN EXAMPLE
+    areas: 'https://raw.githubusercontent.com/img-mapper/react-docs/master/src/assets/example.json',
   };
   
-  export default Map;
+  return <ImageMapper src={URL} map={MAP} />
+}
+
+export default Map;
