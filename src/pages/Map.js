53d54f65b5482cb97c2d1acffa032da616261c3f
neotokyounicorn@gmail.com <neotokyounicorn@gmail.com>
2022-06-03 01:29:39 +0200

    map overlay

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ImageMapper from 'react-img-mapper';
+import Map1 from '../assets/map.jpg';
+import Areas1 from '../assets/map.json';
 
-const Map = props => {
-  const URL = 'https://raw.githubusercontent.com/img-mapper/react-docs/master/src/assets/example.jpg';
+const Map = () => {
+//  const URL = 'https://raw.githubusercontent.com/img-mapper/react-docs/master/src/assets/example.jpg';
+  const URL = Map1;
   const MAP = {
     name: 'Map',
     // GET JSON FROM BELOW URL AS AN EXAMPLE
-    areas: 'https://raw.githubusercontent.com/img-mapper/react-docs/master/src/assets/example.json',
+    areas: Areas1,
   };
-  
-  return <ImageMapper src={URL} map={MAP} />
+
+  const LoadLevel = () => {
+    alert();
+  };
+
+      return (
+
+      <ImageMapper src={URL} map={MAP} onClick={LoadLevel}/>
+
+  )
 }
 
 export default Map;
