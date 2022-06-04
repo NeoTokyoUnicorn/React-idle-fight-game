b4b126ef34894f9b55d5369efce11bb41b79d7a7
neotokyounicorn@gmail.com <neotokyounicorn@gmail.com>
2022-06-04 21:56:36 +0200

    button not working

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react'
 import ReactPlayer from 'react-player'
 import Intro1 from '../assets/intro.mp4';
+import button1 from '../assets/buttonPlay.gif';
+import Map from "./Map";
 
-class Splash  extends Component {
+const LoadMap = () => {
+    this.props.history.push(Map);
+}
+
+class Splash extends Component {
     render () {
         return (
-        <div className='player-wrapper'>
+        <div><div className='player-wrapper'>
             <ReactPlayer
             className='react-player fixed-bottom'
             url= {Intro1}
             width='70%'
             height='70%'
             controls = {false}
-            Loop = {true}
+            loop = {false}
             playing = {true}
             muted = {true}
-
             />
+        </div>
+        <div onClick={LoadMap}><img src={button1} alt='Play game'></img>     
+        </div>
         </div>
         )
     }
