61a2cd78b54c10f509275765c42a8e019c90d7fd
neotokyounicorn@gmail.com <neotokyounicorn@gmail.com>
2022-06-05 22:44:13 +0200

    splash button working

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -3,22 +3,25 @@ import ImageMapper from 'react-img-mapper';
 import Map1 from '../assets/map.jpg';
 import Areas1 from '../assets/map.json';
 
-const Map = () => {
+const clickMap = (props) =>  {
+  alert('a');
+}
+
+const Map = props => {
 //  const URL = 'https://raw.githubusercontent.com/img-mapper/react-docs/master/src/assets/example.jpg';
   const URL = Map1;
   const MAP = {
     name: 'Map',
     // GET JSON FROM BELOW URL AS AN EXAMPLE
     areas: Areas1,
+    onclick: {clickMap}
   };
-
 
   return (
       
       <ImageMapper 
         src={URL} 
         map={MAP} 
-        onClick={area => this.clicked(area)}
       />
 
   )
--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -2,28 +2,29 @@ import React, { Component } from 'react'
 import ReactPlayer from 'react-player'
 import Intro1 from '../assets/intro.mp4';
 import button1 from '../assets/buttonPlay.gif';
-import Map from "./Map";
 
 const LoadMap = () => {
-    this.props.history.push(Map);
+    alert('pushed');
+    window.location.href = "./Map";
 }
 
 class Splash extends Component {
-    render () {
+
+     render () {
         return (
         <div><div className='player-wrapper'>
             <ReactPlayer
             className='react-player fixed-bottom'
             url= {Intro1}
-            width='70%'
-            height='70%'
+            width='50%'
+            height='50%'
             controls = {false}
             loop = {false}
             playing = {true}
             muted = {true}
             />
         </div>
-        <div onClick={LoadMap}><img src={button1} alt='Play game'></img>     
+        <div><button>< img src={button1} alt="Play" onClick={LoadMap} />Play</button>     
         </div>
         </div>
         )
