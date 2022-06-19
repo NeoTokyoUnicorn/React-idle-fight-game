a204c08867ce87fedcf6e55b70e41fe3d3e70c84
neotokyounicorn@gmail.com <neotokyounicorn@gmail.com>
2022-06-19 16:16:36 +0200

    Map click working

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -3,25 +3,34 @@ import ImageMapper from 'react-img-mapper';
 import Map1 from '../assets/map.jpg';
 import Areas1 from '../assets/map.json';
 
-const clickMap = (props) =>  {
-  alert('a');
+const LoadLevel = (props) => {
+  switch (props) {
+    case '1':
+      window.location.href = "../App";
+      break
+    case '29':
+      window.location.href = "../App";
+      break
+    default:
+      return null
+  }
 }
 
 const Map = props => {
 //  const URL = 'https://raw.githubusercontent.com/img-mapper/react-docs/master/src/assets/example.jpg';
   const URL = Map1;
   const MAP = {
-    name: 'Map',
+    name: 'Motavia',
     // GET JSON FROM BELOW URL AS AN EXAMPLE
-    areas: Areas1,
-    onclick: {clickMap}
+    areas: Areas1
   };
 
   return (
       
       <ImageMapper 
         src={URL} 
-        map={MAP} 
+        map={MAP}
+        onClick={(areas) => LoadLevel(areas.name)}
       />
 
   )
